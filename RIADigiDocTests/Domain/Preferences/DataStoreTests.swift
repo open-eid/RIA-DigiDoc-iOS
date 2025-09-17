e898f9708a28abb82161cf7efc7fb27694688284
Markus Tamm <markus.tamm@nortal.com>
2025-09-17 12:01:58 +0300

    Add Theme Chooser unit tests

--- RIADigiDocTests/Domain/Preferences/DataStoreTests.swift
+++ RIADigiDocTests/Domain/Preferences/DataStoreTests.swift
@@ -7,7 +7,7 @@ struct DataStoreTests {
         dataStore = DataStore()
     }
 
-    // MARK: - Tests
+    // MARK: - Language Tests
 
     @Test
     func getSelectedLanguage_success() async throws {
@@ -16,11 +16,26 @@ struct DataStoreTests {
         #expect(allowedLanguageCodes.contains(selectedLanguage))
     }
 
-    @Test func setSelectedLanguage_success() async throws {
+    @Test
+    func setSelectedLanguage_success() async throws {
         let testLanguageCode: String = "et"
-
         await dataStore.setSelectedLanguage(newLanguageCode: testLanguageCode)
-
         #expect(await dataStore.getSelectedLanguage() == testLanguageCode)
+    }
+
+    // MARK: - Theme Tests
+
+    @Test
+    func getSelectedTheme_success() async throws {
+        let allowedThemeRawValues: [Int] = [0, 1, 2]
+        let selectedTheme: Int = await dataStore.getSelectedTheme()
+        #expect(allowedThemeRawValues.contains(selectedTheme))
+    }
+
+    @Test
+    func setSelectedTheme_success() async throws {
+        let testThemeRawValue: Int = 2
+        await dataStore.setSelectedTheme(testThemeRawValue)
+        #expect(await dataStore.getSelectedTheme() == testThemeRawValue)
     }
 }
