3b208f0d7744ed7f2d9db432d59c006059d20bcf
Boriss Melikjan <borissmelikjan@gmail.com>
2026-03-05 09:02:24 +0200

    WebEidLib: DI, Auth and Sign Services, Utils, Exceptions. (#159)
    * NFC-129 WebEidLib: DI, Auth and Sign Services, Utils, Exceptions.
    * NFC-129 Refactor base64DecodeFlexible function.
    * NFC-129 camelCase for request request fields.
    * NFC-129 SwiftLint warnings fixes.

---
+++ Modules/WebEidLib/Package.swift
@@ -0,0 +1,57 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WebEidLib",
+    platforms: [.iOS(.v18)],
+    products: [
+        .library(
+            name: "WebEidLib",
+            targets: ["WebEidLib"]
+        ),
+        .library(name: "WebEidLibMocks", targets: ["WebEidLibMocks"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/filom/ASN1Decoder", exact: .init(1, 10, 0)),
+        .package(url: "https://github.com/hmlongco/Factory", exact: .init(2, 5, 3)),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: .init(5, 10, 2)),
+        .package(path: "../UtilsLib"),
+        .package(path: "../CommonsLib")
+    ],
+    targets: [
+        .target(
+            name: "WebEidLib",
+            dependencies: [
+                "Alamofire",
+                "UtilsLib",
+                "CommonsLib",
+                .product(name: "FactoryKit", package: "Factory")
+            ],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency"),
+                .enableUpcomingFeature("SendableByDefault"),
+                .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
+                .enableUpcomingFeature("InferIsolatedConformances")
+            ]
+        ),
+        .target(
+            name: "WebEidLibMocks",
+            dependencies: ["WebEidLib"],
+            path: "Tests/Mocks/Generated"
+        ),
+        .testTarget(
+            name: "WebEidLibTests",
+            dependencies: [
+                "WebEidLib",
+                "WebEidLibMocks",
+                "UtilsLib",
+                "CommonsLib",
+                .product(name: "UtilsLibMocks", package: "utilslib"),
+                .product(name: "CommonsLibMocks", package: "commonslib"),
+                .product(name: "FactoryTesting", package: "Factory")
+            ]
+        )
+    ]
+)
