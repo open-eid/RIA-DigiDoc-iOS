81d84b99f628220df93f5903b1be0e5c16d2b113
Marten Rebane <marten.rebane@nortal.com>
2026-03-20 15:12:02 +0200

    Change copyright year

--- Modules/IdCardLib/Sources/bR301/shim.c
+++ Modules/IdCardLib/Sources/bR301/shim.c
@@ -3,7 +3,7 @@
 //  IdCardLib
 //
 /*
- * Copyright 2017 - 2025 Riigi Infosüsteemi Amet
+ * Copyright 2017 - 2026 Riigi Infosüsteemi Amet
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
